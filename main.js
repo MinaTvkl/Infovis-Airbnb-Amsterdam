cd6171968367254615df49935b28511f70338f13
Thomas van T <thomasvant@gmail.com>
2019-11-13 16:50:09 +0000

    X axis now showing

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
-var dataset;
+var dataset, districtNames;
 
 d3.csv("bar_chart_migration_saldo.csv").then(function(data) {
-  dataset = removeFirst(Object.values(data[1])).map(function(value) {
-    return parseInt(value);
-  });
+  districtNames = removeFirst(Object.keys(data[0]));
+  dataset = removeFirst(Object.values(data[1])).map(value => parseInt(value));
   gen_vis();
 });
 
 function removeFirst(array) {
-  return array.splice(1,array.length);
+  return array.splice(1, array.length);
 }
 
 var idiomWidth = 500;
-var idiomHeight = 400;
+var idiomHeight = 300;
 
-var padding = {top : 20, right: 20, bottom: 20, left: 40}
+var padding = {
+  top: 20,
+  right: 20,
+  bottom: 60,
+  left: 40
+}
 
 var graphWidth = idiomWidth - padding.left - padding.right;
 var graphHeight = idiomHeight - padding.top - padding.bottom;
 
 //Used https://bl.ocks.org/gurjeet/83189e2931d053187eab52887a870c5e as example
 function gen_vis() {
-  var barWidth = Math.abs(graphWidth / dataset.length) ;
+  var barWidth = Math.abs(graphWidth / dataset.length);
   var maxBarPositiveHeightPercentage = d3.max(dataset) / (d3.max(dataset) + Math.abs(d3.min(dataset)));
-  console.log(maxBarPositiveHeightPercentage);
+
   var maxBarPositiveHeight = graphHeight * maxBarPositiveHeightPercentage;
   var maxBarNegativeHeight = graphHeight * (1 - maxBarPositiveHeightPercentage);
 
@@ -35,7 +39,11 @@ function gen_vis() {
     .domain([d3.min(dataset), d3.max(dataset)])
     .range([graphHeight, 0]);
 
+  var wholeXAxisScale = d3.scaleBand().domain(districtNames).range([0,graphWidth]);
+
   var yAxis = d3.axisLeft(wholeYAxisScale);
+
+  var xAxis = d3.axisBottom(wholeXAxisScale).tickSize(0);
 
   var svg = d3.select("#chart")
     .append("svg")
@@ -44,16 +52,29 @@ function gen_vis() {
     .style("border", "1px solid");
 
   svg.selectAll("rect")
-  .data(dataset)
-  .enter()
-  .append("rect")
-    .attr("x", function(value, index) {return padding.left + index*barWidth;})
-    .attr("y", function(value) {return padding.top + maxBarPositiveHeight - Math.max(0, positiveYAxisScale(value));})
-    .attr("height", function(value) {return Math.abs(positiveYAxisScale(value));})
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("x", function(value, index) {
+      return padding.left + index * barWidth;
+    })
+    .attr("y", function(value) {
+      return padding.top + maxBarPositiveHeight - Math.max(0, positiveYAxisScale(value));
+    })
+    .attr("height", function(value) {
+      return Math.abs(positiveYAxisScale(value));
+    })
     .attr("width", barWidth)
     .style("fill", "green");
 
   svg.append("g").attr("transform", function(d) {
     return "translate(" + padding.left + ", " + padding.top + ")";
   }).call(yAxis);
+
+  svg.append("g")
+    .attr('class','axis')
+    .call(xAxis)
+    .attr("transform", "translate(" + padding.left +  "," + (maxBarPositiveHeight + padding.top) + ")")
+    .selectAll("text").attr("transform", "translate(-20, " + (maxBarNegativeHeight + 20 )+ ") rotate(-45)");
+
 }
