cfa0a42ea3c6494107a3573852d7543c069b258c
Thomas van T <thomasvant@gmail.com>
2019-11-23 15:27:55 +0000

    Both graphs now in one function

--- main.js
+++ main.js
@@ -83,21 +83,44 @@ function gen_vis() {
     .attr("transform", "translate(" + axesSpace.left + "," + (barchart_posHeight + axesSpace.top) + ")")
     .selectAll("text").attr("transform", "translate(" + -1 * (barchart_barWidth / 4) + ", " + (barchart_negHeight + 20) + ") rotate(-45)");
 
-  d3.select("#year-slider").on("input", function() {
-    curYear = document.getElementById("year-slider").value;
-    barchart_dataset = barchart_datasetValues[curYear - 1];
 
-    barchart_svg.selectAll("rect").data(barchart_dataset)
-      .transition().duration(1000)
-      .attr("x", function(value, index) {
-        return axesSpace.left + index * barchart_barWidth;
-      }).attr("y", function(value) {
-        return barchart_posHeight - Math.max(0, barchart_posYScale(value));
-      }).attr("height", function(value) {
-        return Math.abs(barchart_posYScale(value));
-      }).attr("width", barchart_barWidth).style("fill", "green");
+  //Linechart
+  var linechart_datasetYears = [2015, 2016, 2017, 2018, 2019];
+  var linechart_dataset = linechart_datasetValues[curDistrict];
+
+  var linechart_min = getMin(linechart_datasetValues);
+  var linechart_max = getMax(linechart_datasetValues);
+
+  var linechart_xScale = d3.scaleLinear().domain([linechart_datasetYears[0], linechart_datasetYears[linechart_datasetYears.length - 1]]).range([0, chartWidth]);
+  var linechart_yScale = d3.scaleLinear().domain([linechart_min, linechart_max]).range([chartHeight, 0]);
+
+  var linechart_line = d3.line().x(function(value, index) {
+    return linechart_xScale(index + linechart_datasetYears[0]);
+  }).y(function(value) {
+    return linechart_yScale(value);
   });
 
+  var linechart_svg = d3.select("#line-chart").append("svg")
+    .attr("height", idiomHeight).attr("width", idiomWidth)
+    .style("border", "1px solid");
+
+  linechart_svg.append("g").call(d3.axisBottom(linechart_xScale).ticks(linechart_datasetYears.length).tickFormat(d3.format("d")))
+    .attr("transform", "translate(" + axesSpace.left + "," + (chartHeight + axesSpace.top) + ")");
+
+  linechart_svg.append("g").call(d3.axisLeft(linechart_yScale))
+    .attr("transform", "translate(" + axesSpace.left + "," + axesSpace.top + ")");
+
+  linechart_svg.append("path").datum(linechart_dataset)
+    .attr("class", "line").attr("d", linechart_line)
+    .attr("transform", "translate(" + axesSpace.left + "," + axesSpace.top + ")");
+
+  linechart_svg.selectAll(".dot").data([linechart_dataset[curYear]]).enter().append("circle").attr("class", "dot")
+    .attr("cx", function(value) {
+      return linechart_xScale(curYear)
+    }).attr("cy", function(value) {
+      return linechart_yScale(linechart_dataset[curYear - linechart_datasetYears[0]])
+    }).attr("r", 7)
+    .attr("transform", "translate(" + axesSpace.left + "," + axesSpace.top + ")");
 }
 
 function getMax(datasetValues) {
